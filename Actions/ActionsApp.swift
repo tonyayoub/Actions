9e0046acf9346e33742641c00bc0384832492d8d
Tony Ayoub <tony.ayoub@daresay.co>
2023-10-20 15:36:07 +0200

    Initial Commit

---
+++ Actions/ActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActionsApp.swift
+//  Actions
+//
+//  Created by Tony Ayoub on 2023-10-20.
+//
+
+import SwiftUI
+
+@main
+struct ActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Actions/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Actions
+//
+//  Created by Tony Ayoub on 2023-10-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
